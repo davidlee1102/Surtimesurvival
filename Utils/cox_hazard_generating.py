a0319af76c06090e18e883cfc796b0b26f369d52
davidlee1102 <lenhathungdev112@gmail.com>
2023-07-05 17:31:54 +0100

    adding failure function

---
+++ Utils/cox_hazard_generating.py
@@ -0,0 +1,13 @@
+
+def input_features(features: list):
+    assert type(features) is not list, "features should be in list"
+    return features
+
+
+def input_beta(beta: list):
+    assert type(beta) is not list, "beta should be in list"
+    return beta
+
+def time_failure_function(t):
+    failure_function =
+    return
