c4d583977e29e57a588948ab2eb1b57c8e24dbea
davidlee1102 <lenhathungdev112@gmail.com>
2023-07-10 17:24:13 +0100

    Fix error from config

--- SurvTRACE/survtrace/train_utils.py
+++ SurvTRACE/survtrace/train_utils.py
@@ -274,7 +274,7 @@ class Trainer:
 
     def train_single_event(self,
                            train_set,
-                           masks_train,
+                           masks_train=None,
                            val_set=None,
                            masks_val=None,
                            batch_size=64,
@@ -285,119 +285,207 @@ class Trainer:
                            **kwargs,
                            ):
 
-        df_train, df_y_train = train_set
-        durations_train, events_train = self.get_target(df_y_train)
-
-        print()
-
-        if val_set is not None:
-            df_val, df_y_val = val_set
-            durations_val, events_val = self.get_target(df_y_val)
-
-            # tensor_val = torch.tensor(val_set[0].values)
-            # tensor_y_val = torch.tensor(val_set[1].values)
-
-            # edit here
-            tensor_val = val_set[0]
-            tensor_y_val = torch.tensor(val_set[1].values)
-            tensor_masks_val = masks_val
-
-        if self.use_gpu:
-            tensor_val = tensor_val.cuda()
-            tensor_y_val = tensor_y_val.cuda()
-
-            # edit here
-            tensor_masks_val = tensor_masks_val.cuda()
-
-        # assign no weight decay on these parameters
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        param_optimizer = list(self.model.named_parameters())
-        optimizer_grouped_parameters = [
-            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
-             'weight_decay': weight_decay},
-            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
-        ]
-        optimizer = BERTAdam(optimizer_grouped_parameters,
-                             learning_rate,
-                             weight_decay_rate=weight_decay,
-                             )
-
-        if val_set is not None:
-            # take early stopping
-            self.early_stopping = EarlyStopping(patience=self.model.config['early_stop_patience'])
-
-        num_train_batch = int(np.ceil(len(df_y_train) / batch_size))
-        train_loss_list, val_loss_list = [], []
-        for epoch in range(epochs):
-            epoch_loss = 0
-            self.model.train()
-
-            # df_train = train_set[0].sample(frac=1)
-
-            # edit here
-            df_y_train = train_set[1]
-
-            # tensor_train = torch.tensor(df_train.values)
-            # tensor_y_train = torch.tensor(df_y_train.values)
-
-            tensor_train = df_train
-            tensor_y_train = torch.tensor(df_y_train.values)
-            tensor_masks_train = masks_train
-
-            if self.use_gpu:
-                tensor_y_train = tensor_y_train.cuda()
-                tensor_train = tensor_train.cuda()
-                tensor_masks_train = tensor_masks_train.cuda()
-
-            for batch_idx in range(num_train_batch):
-                optimizer.zero_grad()
-
-                batch_train = tensor_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
-                batch_y_train = tensor_y_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
-
-                # edit here
-                batch_masks_train = tensor_masks_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
-
-                batch_x_cat = batch_train[:, :self.model.config.num_categorical_feature].long()
-                batch_x_num = batch_train[:, self.model.config.num_categorical_feature:].float()
-
-                phi = self.model(input_ids=batch_x_cat, input_nums=batch_x_num, our_mask=batch_masks_train)
-
-                if len(self.metrics) == 1:  # only NLLPCHazardLoss is asigned
-                    batch_loss = self.metrics[0](phi[1], batch_y_train[:, 0].long(), batch_y_train[:, 1].long(),
-                                                 batch_y_train[:, 2].float(), reduction="mean")
-
-                else:
-                    raise NotImplementedError
-
-                batch_loss.backward()
-                optimizer.step()
-
-                epoch_loss += batch_loss.item()
-
-            train_loss_list.append(epoch_loss / (batch_idx + 1))
+        if masks_train is not None:
+            df_train, df_y_train = train_set
+            durations_train, events_train = self.get_target(df_y_train)
 
             if val_set is not None:
-                self.model.eval()
-                with torch.no_grad():
-                    try:
-                        phi_val = self.model.predict(tensor_val, val_batch_size, tensor_masks_val)
-                    except Exception as E:
-                        print("ERROR")
+                df_val, df_y_val = val_set
+                durations_val, events_val = self.get_target(df_y_val)
 
-                val_loss = self.metrics[0](phi_val, tensor_y_val[:, 0].long(), tensor_y_val[:, 1].long(),
-                                           tensor_y_val[:, 2].float())
-                print("[Train-{}]: {}".format(epoch, epoch_loss))
-                print("[Val-{}]: {}".format(epoch, val_loss.item()))
-                val_loss_list.append(val_loss.item())
-                self.early_stopping(val_loss.item(), self.model, name=self.ckpt)
-                if self.early_stopping.early_stop:
-                    print(f"early stops at epoch {epoch + 1}")
-                    # load best checkpoint
-                    self.model.load_state_dict(torch.load(self.ckpt))
-                    return train_loss_list, val_loss_list
-            else:
-                print("[Train-{}]: {}".format(epoch, epoch_loss))
+                # tensor_val = torch.tensor(val_set[0].values)
+                # tensor_y_val = torch.tensor(val_set[1].values)
+
+                # edit here
+                tensor_val = val_set[0]
+                tensor_y_val = torch.tensor(val_set[1].values)
+                tensor_masks_val = masks_val
+
+            if self.use_gpu:
+                tensor_val = tensor_val.cuda()
+                tensor_y_val = tensor_y_val.cuda()
+
+                # edit here
+                tensor_masks_val = tensor_masks_val.cuda()
+
+            # assign no weight decay on these parameters
+            no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+            param_optimizer = list(self.model.named_parameters())
+            optimizer_grouped_parameters = [
+                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+                 'weight_decay': weight_decay},
+                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+            optimizer = BERTAdam(optimizer_grouped_parameters,
+                                 learning_rate,
+                                 weight_decay_rate=weight_decay,
+                                 )
+
+            if val_set is not None:
+                # take early stopping
+                self.early_stopping = EarlyStopping(patience=self.model.config['early_stop_patience'])
+
+            num_train_batch = int(np.ceil(len(df_y_train) / batch_size))
+            train_loss_list, val_loss_list = [], []
+            for epoch in range(epochs):
+                epoch_loss = 0
+                self.model.train()
+
+                # df_train = train_set[0].sample(frac=1)
+
+                # edit here
+                df_y_train = train_set[1]
+
+                # tensor_train = torch.tensor(df_train.values)
+                # tensor_y_train = torch.tensor(df_y_train.values)
+
+                tensor_train = df_train
+                tensor_y_train = torch.tensor(df_y_train.values)
+                tensor_masks_train = masks_train
+
+                if self.use_gpu:
+                    tensor_y_train = tensor_y_train.cuda()
+                    tensor_train = tensor_train.cuda()
+                    tensor_masks_train = tensor_masks_train.cuda()
+
+                for batch_idx in range(num_train_batch):
+                    optimizer.zero_grad()
+
+                    batch_train = tensor_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
+                    batch_y_train = tensor_y_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
+
+                    # edit here
+                    batch_masks_train = tensor_masks_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
+
+                    batch_x_cat = batch_train[:, :self.model.config.num_categorical_feature].long()
+                    batch_x_num = batch_train[:, self.model.config.num_categorical_feature:].float()
+
+                    phi = self.model(input_ids=batch_x_cat, input_nums=batch_x_num, our_mask=batch_masks_train)
+
+                    if len(self.metrics) == 1:  # only NLLPCHazardLoss is asigned
+                        batch_loss = self.metrics[0](phi[1], batch_y_train[:, 0].long(), batch_y_train[:, 1].long(),
+                                                     batch_y_train[:, 2].float(), reduction="mean")
+
+                    else:
+                        raise NotImplementedError
+
+                    batch_loss.backward()
+                    optimizer.step()
+
+                    epoch_loss += batch_loss.item()
+
+                train_loss_list.append(epoch_loss / (batch_idx + 1))
+
+                if val_set is not None:
+                    self.model.eval()
+                    with torch.no_grad():
+                        try:
+                            phi_val = self.model.predict(tensor_val, val_batch_size, tensor_masks_val)
+                        except Exception as E:
+                            print("ERROR")
+
+                    val_loss = self.metrics[0](phi_val, tensor_y_val[:, 0].long(), tensor_y_val[:, 1].long(),
+                                               tensor_y_val[:, 2].float())
+                    print("[Train-{}]: {}".format(epoch, epoch_loss))
+                    print("[Val-{}]: {}".format(epoch, val_loss.item()))
+                    val_loss_list.append(val_loss.item())
+                    self.early_stopping(val_loss.item(), self.model, name=self.ckpt)
+                    if self.early_stopping.early_stop:
+                        print(f"early stops at epoch {epoch + 1}")
+                        # load best checkpoint
+                        self.model.load_state_dict(torch.load(self.ckpt))
+                        return train_loss_list, val_loss_list
+                else:
+                    print("[Train-{}]: {}".format(epoch, epoch_loss))
+        else:
+
+            df_train, df_y_train = train_set
+            durations_train, events_train = self.get_target(df_y_train)
+
+            if val_set is not None:
+                df_val, df_y_val = val_set
+                durations_val, events_val = self.get_target(df_y_val)
+                tensor_val = torch.tensor(val_set[0].values)
+                tensor_y_val = torch.tensor(val_set[1].values)
+
+            if self.use_gpu:
+                tensor_val = tensor_val.cuda()
+                tensor_y_val = tensor_y_val.cuda()
+
+            # assign no weight decay on these parameters
+            no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+            param_optimizer = list(self.model.named_parameters())
+            optimizer_grouped_parameters = [
+                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+                 'weight_decay': weight_decay},
+                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+            ]
+            optimizer = BERTAdam(optimizer_grouped_parameters,
+                                 learning_rate,
+                                 weight_decay_rate=weight_decay,
+                                 )
+
+            if val_set is not None:
+                # take early stopping
+                self.early_stopping = EarlyStopping(patience=self.model.config['early_stop_patience'])
+
+            num_train_batch = int(np.ceil(len(df_y_train) / batch_size))
+            train_loss_list, val_loss_list = [], []
+            for epoch in range(epochs):
+                epoch_loss = 0
+                self.model.train()
+                df_train = train_set[0].sample(frac=1)
+                df_y_train = train_set[1].loc[df_train.index]
+
+                tensor_train = torch.tensor(df_train.values)
+                tensor_y_train = torch.tensor(df_y_train.values)
+                if self.use_gpu:
+                    tensor_y_train = tensor_y_train.cuda()
+                    tensor_train = tensor_train.cuda()
+
+                for batch_idx in range(num_train_batch):
+                    optimizer.zero_grad()
+
+                    batch_train = tensor_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
+                    batch_y_train = tensor_y_train[batch_idx * batch_size:(batch_idx + 1) * batch_size]
+
+                    batch_x_cat = batch_train[:, :self.model.config.num_categorical_feature].long()
+                    batch_x_num = batch_train[:, self.model.config.num_categorical_feature:].float()
+
+                    phi = self.model(input_ids=batch_x_cat, input_nums=batch_x_num)
+
+                    if len(self.metrics) == 1:  # only NLLPCHazardLoss is asigned
+                        batch_loss = self.metrics[0](phi[1], batch_y_train[:, 0].long(), batch_y_train[:, 1].long(),
+                                                     batch_y_train[:, 2].float(), reduction="mean")
+
+                    else:
+                        raise NotImplementedError
+
+                    batch_loss.backward()
+                    optimizer.step()
+
+                    epoch_loss += batch_loss.item()
+
+                train_loss_list.append(epoch_loss / (batch_idx + 1))
+
+                if val_set is not None:
+                    self.model.eval()
+                    with torch.no_grad():
+                        phi_val = self.model.predict(tensor_val, val_batch_size)
+
+                    val_loss = self.metrics[0](phi_val, tensor_y_val[:, 0].long(), tensor_y_val[:, 1].long(),
+                                               tensor_y_val[:, 2].float())
+                    print("[Train-{}]: {}".format(epoch, epoch_loss))
+                    print("[Val-{}]: {}".format(epoch, val_loss.item()))
+                    val_loss_list.append(val_loss.item())
+                    self.early_stopping(val_loss.item(), self.model, name=self.ckpt)
+                    if self.early_stopping.early_stop:
+                        print(f"early stops at epoch {epoch + 1}")
+                        # load best checkpoint
+                        self.model.load_state_dict(torch.load(self.ckpt))
+                        return train_loss_list, val_loss_list
+                else:
+                    print("[Train-{}]: {}".format(epoch, epoch_loss))
 
         return train_loss_list, val_loss_list
 
@@ -513,7 +601,7 @@ class Trainer:
 
     def fit(self,
             train_set,
-            masks_train,
+            masks_train=None,
             val_set=None,
             masks_val=None,
             batch_size=64,
